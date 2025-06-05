6ae37a815d5361de069cf3f4d29b7b242d75eff7
Daniel Jorge Manzano <djorgemanzano@gmail.com>
2025-06-05 11:35:39 -0300

    ta ficando bom

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
-console.log("oi");
+const trocaModo = document.getElementById('mostra-config');
+const configSection = document.getElementById('configuracoes');
+const principal = document.getElementById('principal');
+
+trocaModo.addEventListener('click', () => { // caso o botão seja clicado, a exibição de configurações ou o cronômetro será alternada
+    if (configSection.style.display === 'none' || configSection.style.display === '') { // caso esteja no modo de cronômetro
+        configSection.style.display = 'block'; 
+        principal.style.display = 'none'; 
+        trocaModo.textContent = 'Esconder Configurações';
+    } else { // caso esteja no modo de configurações
+        configSection.style.display = 'none';
+        principal.style.display = 'block'; 
+        trocaModo.textContent = 'Mostrar Configurações';
+    }
+});
+
+
